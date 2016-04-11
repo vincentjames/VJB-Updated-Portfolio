45c3920147b1aef8d340867d189ef3a1d486e038
Vincent Bellittera <bellaproducts78@gmail.com>
2016-04-11 19:41:49 -0400

    Installed bootstrap template

--- index.php
+++ index.php
@@ -1,10 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>VJB Web Design</title>
-</head>
-<body>
-	<p>Welcome to VJB Web Design</p>
-</body>
+  <head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 101 Template</title>
+    <link href="_/css/bootstrap.css" rel="stylesheet">
+    <link href="_/css/mystyles.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+		
+  </head>	
+  <body>
+		
+    <h1>This setup needs to be set up with prepros and then it is all set. Get Coding!</h1>
+
+
+    <script src="_/js/bootstrap.js"></script>
+    <script src="_/js/myscript.js"></script>
+		
+  </body>	
 </html>
